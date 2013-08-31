afa3691497419900003622eb56dee130a3ee0983
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-08-31 16:20:22 +0200

    update script

--- src/new.sh
+++ src/new.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 outdir="$(dirname $0)"
-cp "$outdir/solution_1.h" "$outdir/solution_$1.h"
-cp "$outdir/solution_1.cpp" "$outdir/solution_$1.cpp"
+cp "$outdir/solution_1.h" "$outdir/solution_$1.h" -i
+cp "$outdir/solution_1.cpp" "$outdir/solution_$1.cpp" -i
