53cc99b75603e1ee3f4b9416b66388f930d9a4d6
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-08-31 18:18:29 +0200

    solve 9

---
+++ src/solution_9.h
@@ -0,0 +1,11 @@
+/**********************************
+*  solution_9.h                   *
+**********************************/
+#pragma once
+#include "solution.h"
+class Solution9 : public Solution {
+protected:
+    virtual answer_t get_answer();
+public:
+    Solution9();
+};
