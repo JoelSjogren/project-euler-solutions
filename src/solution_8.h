f8ef6282e4bfae7906f2d8ed241c170cae187ba2
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-08-31 17:58:15 +0200

    solve 8

---
+++ src/solution_8.h
@@ -0,0 +1,11 @@
+/**********************************
+*  solution_8.h                   *
+**********************************/
+#pragma once
+#include "solution.h"
+class Solution8 : public Solution {
+protected:
+    virtual answer_t get_answer();
+public:
+    Solution8();
+};
