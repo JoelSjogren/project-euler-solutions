d37b040670457c4b6c98b0116259558045912f0f
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-10-11 16:43:39 +0200

    solve 11

---
+++ src/solution_0011.h
@@ -0,0 +1,11 @@
+/**********************************
+*  solution_0011.h                *
+**********************************/
+#pragma once
+#include "solution.h"
+class Solution0011 : public Solution {
+protected:
+    virtual answer_t get_answer();
+public:
+    Solution0011();
+};
