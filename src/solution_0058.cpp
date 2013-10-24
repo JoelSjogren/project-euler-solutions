ea2d37c3253a28277abaeffb9594ae719af615e9
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-10-24 20:10:10 +0200

    solve 58

--- src/solution_0058.cpp
+++ src/solution_0058.cpp
@@ -1,11 +1,29 @@
 /**********************************
 *  solution_0058.cpp              *
 **********************************/
-// not finished
 #include "solution_0058.h"
 #include "is_prime.h"
 Solution0058 s0058;
 Solution0058::Solution0058() : Solution(58, this) {};
 answer_t Solution0058::get_answer() {
-    return 0;
+    answer_t side_length = 3;
+    answer_t i = 9;
+    answer_t primes = 3;
+    answer_t non_primes = 2;
+    while (non_primes <= 9 * primes) {
+        i += side_length + 1;
+        if (is_prime(i)) primes++;
+        else non_primes++;
+        i += side_length + 1;
+        if (is_prime(i)) primes++;
+        else non_primes++;
+        i += side_length + 1;
+        if (is_prime(i)) primes++;
+        else non_primes++;
+        i += side_length + 1;
+        if (is_prime(i)) primes++;
+        else non_primes++;
+        side_length += 2;
+    }
+    return side_length;
 }
